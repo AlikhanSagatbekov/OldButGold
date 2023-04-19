00f9f8d1bd28f06cfda8e2649cd645b4ded3a0aa
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-19 23:06:40 +0600

    added the func sort and crate a new func printList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,10 +1,12 @@
-public class MyArrayList<T> implements MyList {
+public class MyArrayList<T extends Comparable<T>> implements MyList {
     private int size = 0;
     private T[] arr;
+
     public MyArrayList() {
         this.arr = (T[]) new Comparable[10];
         this.size = 0;
     }
+
     @Override
     public int size() {
         return size;
@@ -12,8 +14,8 @@ public class MyArrayList<T> implements MyList {
 
     @Override
     public boolean contains(Object o) {
-        for(int i = 0; i < size; i++){
-            if(o.equals(arr[i])){
+        for (int i = 0; i < size; i++) {
+            if (o.equals(arr[i])) {
                 return true;
             }
         }
@@ -22,14 +24,15 @@ public class MyArrayList<T> implements MyList {
 
     @Override
     public void add(Object item) {
-        if(arr.length == size){
+        if (arr.length == size) {
             increaseBuffer();
         }
-        arr[size++] = (T)item;
+        arr[size++] = (T) item;
     }
-    private void increaseBuffer(){
-        T[] newArr = (T[]) new Object[arr.length*2];
-        for(int i = 0; i < size; i++){
+
+    private void increaseBuffer() {
+        T[] newArr = (T[]) new Object[arr.length * 2];
+        for (int i = 0; i < size; i++) {
             newArr[i] = arr[i];
         }
         arr = newArr;
@@ -40,16 +43,17 @@ public class MyArrayList<T> implements MyList {
         if (arr.length == size - 1) {
             increaseBuffer();
         }
-        for(int i = size -1; i >= index; i--){
-            arr[i+1] = arr[i];
+        for (int i = size - 1; i >= index; i--) {
+            arr[i + 1] = arr[i];
         }
         arr[index] = (T) item;
         size++;
     }
+
     @Override
     public boolean remove(Object item) {
-        for(int i = 0; i < size; i++){
-            if(arr[i].equals(item)){
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(item)) {
                 remove(i);
                 size--;
                 return true;
@@ -57,24 +61,26 @@ public class MyArrayList<T> implements MyList {
         }
         return false;
     }
-    private void validIndex(int index){
-        if(index >= size || index < 0){
+
+    private void validIndex(int index) {
+        if (index >= size || index < 0) {
             System.out.println("Error, No such element in the array");
             throw new IndexOutOfBoundsException();
         }
     }
+
     @Override
     public Object remove(int index) {
         validIndex(index);
-        if(size == 1){
+        if (size == 1) {
             T temp = arr[0];
             size = 0;
             clear();
             return temp;
         }
         T temp = arr[index];
-        for(int i = index; i < size-1; i++){
-            arr[i] = arr[i+1];
+        for (int i = index; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
         }
         size--;
         return temp;
@@ -95,8 +101,8 @@ public class MyArrayList<T> implements MyList {
 
     @Override
     public int indexOf(Object o) {
-        for(int i = 0; i < size; i++){
-            if(arr[i].equals(o)){
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(o)) {
                 return i;
             }
         }
@@ -107,12 +113,12 @@ public class MyArrayList<T> implements MyList {
     @Override
     public int lastIndexOf(Object o) {
         int desiredIndex = -1;
-        for(int i = 0; i < size; i++){
-            if(arr[i].equals(o)){
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(o)) {
                 desiredIndex = i;
             }
         }
-        if(desiredIndex == -1){
+        if (desiredIndex == -1) {
             System.out.println("Error, No such element in the array");
         }
         return desiredIndex;
@@ -120,6 +126,19 @@ public class MyArrayList<T> implements MyList {
 
     @Override
     public void sort() {
-
+        for(int i = 0; i < size - 1;i++){
+            for (int j = 0; j < size - i - 1; j++){
+                if (arr[j].compareTo(arr[j+1]) > 0){
+                    T temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public void printList(){
+        for(int i = 0; i < size; i++){
+            System.out.print(arr[i] +  " ");
+        }
     }
 }
