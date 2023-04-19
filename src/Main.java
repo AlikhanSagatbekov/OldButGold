ac158396ed3469c4fb247d245a9a06bc7be6a71f
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-19 23:58:41 +0600

    chec methods.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import org.w3c.dom.Node;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(10);
+        myLinkedList.add(5);
+        myLinkedList.add(9);
