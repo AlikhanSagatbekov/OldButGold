5965a40f3d55735a404ea77c8df3a47b57569b2a
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-30 23:28:16 +0600

    Start work with MyArrayListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -2,4 +2,7 @@ import java.util.NoSuchElementException;
 public class MyArrayListQueue <T extends Comparable<T>> {
     MyArrayList queue;
 
+    public MyArrayListQueue() {
+        queue = new MyArrayList();
+    }
 }
--- src/MyLikedList.java
+++ src/MyLikedList.java
@@ -1,24 +1,21 @@
 import java.util.EmptyStackException;
 
-public class MyLinkedList <E extends Comparable<E>> implements MyList {
+public class MyLinkedList <E extends Comparable<E>> implements MyList{
 
     private int size = 0;
 
-    private class Node<E> {
+    private class Node<E>{
 
         private E data;
         Node<E> next;
         Node<E> previous;
-
-        public Node(E data) {
+        public Node(E data){
             this.data = data;
         }
-
-        public Node() {
+        public Node(){
 
         }
     }
-
     private Node head;
     private Node tail;
 
@@ -27,11 +24,11 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
         return size;
     }
 
-    @Override
+    @Override // finding the certain element in the linked list
     public boolean contains(Object o) {
         Node currentNode = this.head;
-        while (currentNode != null) {
-            if (currentNode.data.equals((E) o)) {
+        while(currentNode != null){ // the logic of traversing the linked list
+            if(currentNode.data.equals((E)o)){
                 return true;
             }
             currentNode = currentNode.next;
@@ -40,41 +37,41 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
     }
 
     @Override
-    public void add(Object item) {
-        Node newNode = new Node<>((E) item);
-        if (this.head == null) {
+    public void add(Object item) { // adding the element in the end of the linked list
+        Node newNode = new Node<>((E)item);
+        if(this.head == null){
             this.head = newNode;
             this.tail = newNode;
             size++;
             return;
         }
         Node currentNode = this.head;
-        while (currentNode.next != null) {
+        while(currentNode.next != null){
             currentNode = currentNode.next;
         }
         currentNode.next = newNode;
-        newNode.previous = currentNode;
+        newNode.previous = currentNode; // identifying the precious node
         this.tail = newNode;
         size++; // increasing the size
     }
 
     @Override
-    public void add(Object item, int index) {
+    public void add(Object item, int index) { // a method to add the element by index
         Node currentNode = this.head;
         Node newNode = new Node<>((E) item);
         int currentIndex = 0;
-        if (index == 0) {
+        if(index == 0){
             Node temp = this.head;
             this.head = newNode;
             this.head.next = temp;
             return;
-        } else if (index >= size) {
-            add((E) item);
+        }else if(index >= size){
+            add((E)item);
             return;
         }
 
-        while (currentNode != null) {
-            if (currentIndex == index) {
+        while(currentNode != null){
+            if(currentIndex == index){
                 currentNode.previous.next = newNode;
                 newNode.next = currentNode;
                 size++;
@@ -88,16 +85,16 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
     }
 
     @Override
-    public boolean remove(Object item) {
+    public boolean remove(Object item) { // removing the first appearance of the object
         Node currentNode = this.head;
         Node previousNode = new Node();
-        if (currentNode != null && currentNode.data.equals((E) item)) {
+        if(currentNode != null && currentNode.data.equals((E)item)){
             this.head = currentNode.next;
             size--;
             return true;
         }
-        while (currentNode != null) {
-            if (currentNode.data.equals((E) item)) {
+        while(currentNode != null){//traversing through the List
+            if(currentNode.data.equals((E)item)){
                 System.out.println("Starting...");
                 break;
             }
@@ -105,7 +102,7 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
             currentNode = currentNode.next;
 
         }
-        if (currentNode == null) {
+        if (currentNode == null){
             return false;
         }
 
@@ -115,16 +112,16 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
     }
 
     @Override
-    public Object remove(int index) {
+    public Object remove(int index) { // Removing the item by the index
         int currentIndex = 0;
         Node currentNode = this.head;
         Node previousNode = this.head;
-        if (index == 0) {
+        if(index == 0){
             this.head = currentNode.next;
             return currentNode;
         }
-        while (currentNode != null) {
-            if (index == currentIndex) {
+        while(currentNode != null){
+            if(index == currentIndex){
                 previousNode.next = currentNode.next;
                 size--;
                 return currentNode.data;
@@ -138,17 +135,17 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
     }
 
     @Override
-    public void clear() {
+    public void clear() { // Deleting the linked list
         this.head = new Node<>();
-        this.size = 0;
+        this.size = 0; // the size should be zero now since there no elements
     }
 
     @Override
-    public Object get(int index) {
+    public Object get(int index) {// getting the element by index
         int currentIndex = 0;
         Node currentNode = this.head;
-        while (currentNode != null) {
-            if (index == currentIndex) {
+        while(currentNode != null){
+            if(index == currentIndex){
                 return currentNode.data;
             }
             currentIndex++;
@@ -158,11 +155,11 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
     }
 
     @Override
-    public int indexOf(Object o) {
+    public int indexOf(Object o) {// returns the index of the first appearance of the object
         int index = 0;
         Node currentNode = this.head;
-        while (currentNode != null) {
-            if (currentNode.data.equals((E) o)) {
+        while(currentNode != null) {
+            if(currentNode.data.equals((E) o)){
                 return index;
             }
             index++;
@@ -172,12 +169,12 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
     }
 
     @Override
-    public int lastIndexOf(Object o) {
+    public int lastIndexOf(Object o) { // Returning the last instance of the object in the List
         int index = 0;
         int desiredIndex = -1;
         Node currentNode = this.head;
-        while (currentNode != null) {
-            if (currentNode.data.equals((E) o)) {
+        while(currentNode != null) {
+            if(currentNode.data.equals((E) o)){
                 desiredIndex = index;
             }
             index++;
@@ -187,21 +184,21 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
     }
 
     @Override
-    public void sort() {
-        int n = 0;
+    public void sort() { // sorting the linked list using the bubble sort and changing the data itself
+        int n = 0; // double-checking the size of the array
         Node currentNode = this.head;
-        while (currentNode != null) {
+        while(currentNode != null){
             n++;
             currentNode = currentNode.next;
         }
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             currentNode = this.head;
-            while (currentNode.next != null) {
-                E data1 = (E) currentNode.data;
-                E data2 = (E) currentNode.next.data;
+            while(currentNode.next != null){
+                E data1 = (E)currentNode.data;
+                E data2 = (E)currentNode.next.data;
 
-                if (data1.compareTo(data2) > 0) {
+                if(data1.compareTo(data2) > 0){
                     currentNode.next.data = data1;
                     currentNode.data = data2;
                 }
@@ -212,39 +209,41 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
 
     @Override
     public void addAll(MyList o) {
-        for (int i = 0; i < o.size(); i++) {
+        for(int i = 0; i < o.size(); i++){
             add(o.get(i));
         }
     }
 
-    public Node returnTail() {
+    public Node returnTail(){
         return tail;
     }
 
-    public Node returnHead() {
+    public Node returnHead(){
         return head;
     }
 
-    public void printList() { // Printing the Linked list in order
+    public void printList(){ // Printing the Linked list in order
         Node currentNode = this.head; // the temporary node to access all the nodes
-        while (currentNode != null) { // looping through all the values
+        while(currentNode != null){ // looping through all the values
             System.out.print(currentNode.data + " ");
             currentNode = currentNode.next;
         }
         System.out.println();
     }
-
-    public void printInReverse() {
+    public void printInReverse(){
         Node currentNode = this.tail;
-        while (currentNode != null) {
+        while(currentNode != null){
             System.out.println(currentNode.data + " ");
             currentNode = currentNode.previous;
         }
     }
+
+
     public void SelfAddAll(MyLinkedList list){
         this.tail.next = list.tail;
         this.tail = list.tail;
     }
+
     public E addFirst(Object item){ // adding a new method that will be useful for stack
         if(size == 0 || this.head ==null){
             Node newNode = new Node<>();
@@ -261,6 +260,7 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
             return (E)item;
         }
     }
+
     public boolean isEmpty(){
         if(size == 0 || this.head == null){
             return true;
@@ -268,6 +268,7 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
             return false;
         }
     }
+
     public E removeFirst(){
         Node temp = new Node<>();
         E data = (E) this.head.data;
@@ -275,13 +276,16 @@ public class MyLinkedList <E extends Comparable<E>> implements MyList {
         this.head = temp;
         return data;
     }
+
     public E getFirst(){
         if(this.head != null && size > 0){
             return (E) this.head.data;
         }else{
             throw new EmptyStackException();
         }
+
     }
+
     public E addLast(Object item){
         add(item);
         return (E) this.tail.data;
