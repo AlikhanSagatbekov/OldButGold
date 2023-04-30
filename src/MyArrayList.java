15171f20dcaddb74f7c06f26a5384c035b200177
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-30 23:33:33 +0600

    fix MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,5 +1,11 @@
-public class MyArrayList<T extends Comparable<T>> implements MyList {
-    private int size = 0;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class MyArrayList <T extends Comparable<T>> implements MyList{
+
+    private int size = 0; // this variable must be private by security means.
     private T[] arr;
 
     public MyArrayList() {
@@ -7,44 +13,46 @@ public class MyArrayList<T extends Comparable<T>> implements MyList {
         this.size = 0;
     }
 
-    @Override
+
+    @Override// It returns the size of the array
     public int size() {
         return size;
     }
 
-    @Override
+    @Override// It checks if a certain object is in the arrayList
     public boolean contains(Object o) {
-        for (int i = 0; i < size; i++) {
-            if (o.equals(arr[i])) {
+
+        for(int i = 0; i < size; i++){
+            if(o.equals(arr[i])){
                 return true;
             }
         }
         return false;
     }
 
-    @Override
+    @Override // adding new item
     public void add(Object item) {
-        if (arr.length == size) {
+        if(arr.length == size){
             increaseBuffer();
         }
-        arr[size++] = (T) item;
+        arr[size++] = (T)item;
     }
 
-    private void increaseBuffer() {
-        T[] newArr = (T[]) new Object[arr.length * 2];
-        for (int i = 0; i < size; i++) {
+    private void increaseBuffer(){ // It is a function that increases our array
+        T[] newArr = (T[]) new Object[arr.length*2];
+        for(int i = 0; i < size; i++){
             newArr[i] = arr[i];
         }
         arr = newArr;
     }
 
     @Override
-    public void add(Object item, int index) {
-        if (arr.length == size - 1) {
+    public void add(Object item, int index) { // adding item to a certain index
+        if(arr.length == size-1){
             increaseBuffer();
         }
-        for (int i = size - 1; i >= index; i--) {
-            arr[i + 1] = arr[i];
+        for(int i = size - 1; i >= index; i--){
+            arr[i+1] = arr[i];
         }
         arr[index] = (T) item;
         size++;
@@ -52,8 +60,8 @@ public class MyArrayList<T extends Comparable<T>> implements MyList {
 
     @Override
     public boolean remove(Object item) {
-        for (int i = 0; i < size; i++) {
-            if (arr[i].equals(item)) {
+        for(int i = 0; i < size; i++){
+            if(arr[i].equals(item)){
                 remove(i);
                 size--;
                 return true;
@@ -62,47 +70,39 @@ public class MyArrayList<T extends Comparable<T>> implements MyList {
         return false;
     }
 
-    private void validIndex(int index) {
-        if (index >= size || index < 0) {
-            System.out.println("Error, No such element in the array");
-            throw new IndexOutOfBoundsException();
-        }
-    }
-
-    @Override
+    @Override// this method shifts all the elements by one and the element to delete is overwritten
     public Object remove(int index) {
         validIndex(index);
-        if (size == 1) {
+        if(size == 1){ // found a bug when there is only one element
             T temp = arr[0];
             size = 0;
             clear();
             return temp;
         }
         T temp = arr[index];
-        for (int i = index; i < size - 1; i++) {
-            arr[i] = arr[i + 1];
+        for(int i = index; i < size-1; i++){
+            arr[i] = arr[i+1];
         }
         size--;
         return temp;
     }
 
     @Override
-    public void clear() {
-        T[] newArr = (T[]) new Object[0];
-        arr = newArr;
+    public void clear() { // the method to clear entire List
+        arr = (T[]) new Comparable[10];
         size = 0;
     }
 
     @Override
-    public Object get(int index) {
+    public Object get(int index) { // returning element at the specified index
         validIndex(index);
         return arr[index];
     }
 
     @Override
-    public int indexOf(Object o) {
-        for (int i = 0; i < size; i++) {
-            if (arr[i].equals(o)) {
+    public int indexOf(Object o) { // it searches for the first appearance of the object
+        for(int i = 0; i < size; i++){
+            if(arr[i].equals(o)){
                 return i;
             }
         }
@@ -111,21 +111,21 @@ public class MyArrayList<T extends Comparable<T>> implements MyList {
     }
 
     @Override
-    public int lastIndexOf(Object o) {
+    public int lastIndexOf(Object o) { // it searches for the last entry of the object in array
         int desiredIndex = -1;
-        for (int i = 0; i < size; i++) {
-            if (arr[i].equals(o)) {
+        for(int i = 0; i < size; i++){
+            if(arr[i].equals(o)){
                 desiredIndex = i;
             }
         }
-        if (desiredIndex == -1) {
+        if(desiredIndex == -1){
             System.out.println("Error, No such element in the array");
         }
         return desiredIndex;
     }
 
     @Override
-    public void sort() {
+    public void sort() { // sorting the generic type
         for(int i = 0; i < size - 1;i++){
             for (int j = 0; j < size - i - 1; j++){
                 if (arr[j].compareTo(arr[j+1]) > 0){
@@ -136,15 +136,23 @@ public class MyArrayList<T extends Comparable<T>> implements MyList {
             }
         }
     }
-    public void printList(){
-        for(int i = 0; i < size; i++){
-            System.out.print(arr[i] +  " ");
-        }
-    }
+
     @Override
     public void addAll(MyList o) {
         for(int i = 0; i < o.size(); i++){
             add(o.get(i));
+        }
+    }
+
+    private void validIndex(int index){ // to check if the index is valid
+        if(index < 0){
+            System.out.println("Error, No such element in the array");
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public void printList(){ // adding support method to print all the elements in stdout
+        for(int i = 0; i < size; i++){
+            System.out.print(arr[i] +  " ");
         }
     }
 
