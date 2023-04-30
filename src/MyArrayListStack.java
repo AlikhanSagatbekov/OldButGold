531ea37b7f689c0b03811e2cc8125fa72ca4a5c8
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-30 23:12:13 +0600

    created all methods in MyArrayListStack

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,2 +1,43 @@
-public class MyArrayListStack {
+import java.util.EmptyStackException;
+
+public class MyArrayListStack <T extends Comparable<T>>{
+    MyArrayList stack;
+
+    public MyArrayListStack(){
+        stack = new MyArrayList<>();
+    }
+
+    public T push(Object item){
+        stack.add(item);
+        return (T)item;
+    }
+
+    public T pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return (T) stack.remove(stack.size()-1);
+    }
+
+    public void printStack(){
+        stack.printList();
+        System.out.println();
+    }
+
+    public T peek(){
+        return (T) stack.get(stack.size()-1);
+    }
+
+    public int size(){
+        return stack.size();
+    }
+
+    public boolean isEmpty(){
+        if(stack.size() == 0 || stack.get(0) == null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,2 +1,40 @@
-public class MyLinkedListStack {
+import java.util.LinkedList;
+import java.util.EmptyStackException;
+public class MyLinkedListStack <E extends Comparable<E>>{
+
+    private MyLinkedList stack;
+    public MyLinkedListStack(){
+        stack = new MyLinkedList();
+    }
+
+    public E push(Object item){
+        stack.addFirst(item);
+        return (E) item;
+    }
+
+    public E pop(){
+        if(stack.isEmpty()){
+            throw new EmptyStackException();
+        }
+        return (E) stack.removeFirst();
+
+    }
+
+    public E peek(){
+        return (E) stack.getFirst();
+    }
+
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+
+    public int size(){
+        return stack.size();
+    }
+
+    public void printStack(){
+        stack.printList();
+    }
+
+
 }
