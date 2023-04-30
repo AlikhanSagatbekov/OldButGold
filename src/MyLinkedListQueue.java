2231ff87bb5f828c4e72eab1c10ec960610ed67d
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-30 23:19:05 +0600

    added dequeue

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,14 +1,22 @@
 public class MyLinkedListQueue {
     MyLinkedList queue;
 
-    public MyLinkedListQueue(){
+    public MyLinkedListQueue() {
         queue = new MyLinkedList();
     }
-    public E enqueue(Object item){
+
+    public E enqueue(Object item) {
         return (E) queue.addLast(item);
     }
 
-    public void printQueue(){
+    public void printQueue() {
         queue.printList();
+    }
+
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("No elements in stack");
+        }
+        return (E) queue.removeFirst();
     }
 }
