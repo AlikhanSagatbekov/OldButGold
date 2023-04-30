9b1a78bc8edddbb012723557129d2c7c4875ed06
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-30 23:29:13 +0600

    added enqueue and printQueue to MyArrayListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -5,4 +5,12 @@ public class MyArrayListQueue <T extends Comparable<T>> {
     public MyArrayListQueue() {
         queue = new MyArrayList();
     }
+    public T enqueue(Object item){
+        queue.add(item);
+        return (T) item;
+    }
+    public void printQueue(){
+        queue.printList();
+        System.out.println();
+    }
 }
